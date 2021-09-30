58fe3329b84d5d8bd99bc951ac8f9fe7d192d347
upsu9 <upsu10@gmail.com>
2021-09-30 16:43:05 +0530

    Feat : First component

---
+++ app.js
@@ -0,0 +1,36 @@
+
+var btnTranslate = document.querySelector('#btn-click')
+var txtInput = document.querySelector("#txt-Input")
+var txtOutput = document.querySelector("#output")
+
+
+
+btnTranslate.addEventListener("click", clickHandler) 
+var murl ="https://api.funtranslations.com/translate/minion.json"
+
+
+function getTranslationUrl(text){
+    return murl + "?" + "text=" + text
+
+}
+
+function errorHandler(error)
+{
+console.log('there is an error',error)
+alert("some Error Found!")
+}
+
+function clickHandler(){
+    var inputText = txtInput.value 
+
+
+fetch (getTranslationUrl(inputText))
+.then (response => response.json())
+
+
+.then (json =>{
+ var translatedText = json.contents.translated;
+ txtOutput.innerText = translatedText;
+})
+.catch(errorHandler)
+}
